426dee4545734c2a5666a48fc40378a270e26baf
Lukas Zaruba <lukas.zruba@gmail.com>
2018-01-27 16:57:17 +0100

    Added 404 handler

--- QTasksServer/server.js
+++ QTasksServer/server.js
@@ -19,6 +19,12 @@ function generateContent() {
 generateContent();
 setInterval(generateContent, 30000);
 
+app.use(function(req, res) {
+	res.status(404).send({
+		url : req.originalUrl + ' not found'
+	});
+});
+
 app.listen(port);
 
 console.log('QTasks RESTful API server started on: ' + port);
