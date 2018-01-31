36872bb188ced57e0ba7cd8b7a427c71f46cbb92
Lukas Zaruba <lukas.zruba@gmail.com>
2018-01-31 19:38:02 +0100

    Added priority field and types array

--- QTasksServer/api/controllers/tasksController.js
+++ QTasksServer/api/controllers/tasksController.js
@@ -7,14 +7,29 @@ var Task = require('../models/Task');
 
 var data = new Map();
 
-var projects = ["QTasks UI", "QTasks Testing", "QTasks Server"];
-var titles = ["Write a complimentary note for someone", "Go green – don’t waste paper", "Make a conscious effort to recycle", "Is that litter on the floor? Pick it up and bin it", "Help somebody with a chore they need done!", 
+const projects = ["QTasks UI", "QTasks Testing", "QTasks Server"];
+const titles = ["Write a complimentary note for someone", "Go green – don’t waste paper", "Make a conscious effort to recycle", "Is that litter on the floor? Pick it up and bin it", "Help somebody with a chore they need done!", 
 	"Leave a kind message anywhere (in a library book, on a computer etc.)", "Buy someone a coffee", "We all love surprises! Buy someone an unexpected gift", "Read a good book recently? Pass it on to someone else", "Someone wronged you? Forgive them", 
 	"It’s hard to stay connected – reach out to an elderly person you know", "Lend a friend a book you think they’d like", "Old laptop or mobile lying around? Donate it", "Share your lunch with a friend", "Visit a friend who’s sick"];
-var assignees = ["Jasmine Short", "Joe North", "Sue Ogden", "Adam Bower", "Kimberly North", "Matt Vance"];
+const assignees = ["Jasmine Short", "Joe North", "Sue Ogden", "Adam Bower", "Kimberly North", "Matt Vance"];
+const priorities = ["low", "normal", "high", "critical"]
 
 function getRandomItem(items) {
 	return items[Math.floor(Math.random() * items.length)];
+}
+
+function tryAddType(list, type) {
+	if (Math.random() > 0.5) {
+		list.push(type);
+	}
+}
+
+function getRandomTypes() {
+	var result = [];
+	result.push("ALL_TASKS");
+	tryAddType(result, "MY_TASKS");
+	tryAddType(result, "OVERDUE");
+	return result;
 }
 
 function getRandomDeadline() {
@@ -38,7 +53,7 @@ exports.getTask = function(req, resp) {
 exports.generateNewTask = function() {
 	var taskId = uniqid("task-");
 	var task = new Task(taskId, getRandomItem(projects), getRandomItem(titles), getRandomItem(assignees), getRandomDeadline(),
-			"https://fakedomainforQTasks.com/task/" + taskId);
+			"https://fakedomainforQTasks.com/task/" + taskId, getRandomItem(priorities), getRandomTypes());
 	data.set(taskId, task);
 	if (data.size > 150) {
 		var i = 0;
